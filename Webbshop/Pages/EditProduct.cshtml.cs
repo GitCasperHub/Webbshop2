9cd68993ee87701b238d06514f6bc52f815890fa
GoosePerfect <efraim.kirkhorn@gmail.com>
2021-11-20 13:29:08 +0100

    EditProduct Bug fixes
    Bugfixar och ökad funktionalitet.
    En preview på produkten man ändrar

--- Webbshop/Pages/EditProduct.cshtml.cs
+++ Webbshop/Pages/EditProduct.cshtml.cs
@@ -18,32 +18,51 @@ namespace Webbshop.Pages
         public string SearchId { get; set; } = null;
         public bool IdMatch { get; set; }
         public Product Product { get; set; }
+        public Game Game { get; set; }
+        public GameConsole Console { get; set; }
+        public Gadget Gadget { get; set; }
 
         public string NewName { get; set; }
         public int NewPrice { get; set; }
-        public int NewAgeRestriction { get; set; }
-        public string NewPlatform { get; set; }
         public string NewDescription { get; set; }
         public int NewStock { get; set; }
+        public string NewImageURL { get; set; }
+
+        //Game specific properties
+        public int NewAgeRestriction { get; set; }
+        public string NewPlatform { get; set; }
         public string NewGenre { get; set; }
         public string NewReleaseDate { get; set; }
         public string NewStudio { get; set; }
         public double NewCriticScore { get; set; }
-        public string NewImageURL { get; set; }
+
+        //Console specific properties
+        public string NewConsoleDeveloper { get; set; }
+        public string NewConsoleRelease { get; set; }
+
+        //Gadget specific properties
+        public string NewGadgetPlatform { get; set; }
 
         public string Name { get; set; }
         public int Price { get; set; }
-        public int AgeRestriction { get; set; }
-        public string Platform { get; set; }
         public string Description { get; set; }
         public int Stock { get; set; }
+        public int AgeRestriction { get; set; }
+        public string Platform { get; set; }
+        public string ImageURL { get; set; }
+
+        //Game specific properties
         public string Genre { get; set; }
         public string ReleaseDate { get; set; }
         public string Studio { get; set; }
         public double CriticScore { get; set; }
-        public string ImageURL { get; set; }
 
+        //Console specific properties
+        public string ConsoleDeveloper { get; set; }
+        public string ConsoleRelease { get; set; }
 
+        //Gadget specific properties
+        public string GadgetPlatform { get; set; }
 
         public void OnGet()
         {
@@ -62,24 +81,40 @@ namespace Webbshop.Pages
                 {
                     IdMatch = true;
                     Product = product;
+
                 }
             }
 
             if (saveChanges)
             {
+
                 Product.Name = NewName;
                 Product.Price = NewPrice;
-                //Product.AgeRestriction = NewAgeRestriction;
-                //Product.Platform = NewPlatform;
                 Product.Description = NewDescription;
                 Product.Stock = NewStock;
-                //Product.Genre = NewGenre;
-                //Product.ReleaseDate = NewReleaseDate;
-                //Product.Studio = NewStudio;
-                //Product.CriticScore = NewCriticScore;
                 Product.ImageURL = NewImageURL;
 
-
+                if (SearchId.Substring(0,2) == "GE" )
+                {
+                    Game = (Game)Product;
+                    Game.AgeRestriction = NewAgeRestriction;
+                    Game.Platform = NewPlatform;
+                    Game.Genre = NewGenre;
+                    Game.ReleaseDate = NewReleaseDate;
+                    Game.Studio = NewStudio;
+                    Game.CriticScore = NewCriticScore;
+                }
+                else if(SearchId.Substring(0, 2) == "CE")
+                {
+                    Console = (GameConsole)Product;
+                    Console.ConsoleDeveloper = NewConsoleDeveloper;
+                    Console.ConsoleRelease = NewConsoleRelease;
+                }
+                else
+                {
+                    Gadget = (Gadget)Product;
+                    Gadget.GadgetPlatform = NewGadgetPlatform;
+                }
 
 
                 saveChanges = false;
